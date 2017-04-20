84acb1cf2eb0cbda239cafd5d8f969c3beb98021
christinaxiong <christinaxiong@users.noreply.github.com>
2017-04-20 16:18:39 -0500

    Completed Square Form

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,9 +1,19 @@
 class CalculationsController <ApplicationController #defining a class called CalculationsController and it's going to inherit goodies from Rails framework; render this, it knows to go in views template
+  def square
+    #Parameters: {"num"=>"66"}
+    @user_number = params[:user_number].to_f
+    @square= @user_number**2
+    render("calculations/square.html.erb")
+
+  end
+  def square_form
+    render("calculations/square_form.html.erb")
+    @user_number = params["num"].to_f
+  end
   def flex_square_5
     #Parameters: {"num"=>"5"}
     @user_number = params["num"].to_f
     @square = @user_number **2
     render("calculations/flex_square_5.html.erb")
-
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get("/flexible/square/:num",{:controller =>"calculations", :action => "flex_square_5"}) #:num will turn into a key and hash.
+  get("/square/new",{:controller =>"calculations", :action => "square_form"})
+  get("/square/results",{:controller =>"calculations", :action => "square"})
 end
